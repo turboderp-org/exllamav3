a2ae48308aa813c67058c01f21bd4ab656a21d7a
turboderp <11859846+turboderp@users.noreply.github.com>
2025-04-27 17:05:21 +0200

    Add parallel decoder module

--- exllamav3/modules/__init__.py
+++ exllamav3/modules/__init__.py
@@ -5,4 +5,4 @@ from .rmsnorm import RMSNorm
 from .layernorm import LayerNorm
 from .embedding import Embedding
 from .attn import Attention
-from .transformer import TransformerBlock
+from .transformer import TransformerBlock, ParallelDecoderBlock
