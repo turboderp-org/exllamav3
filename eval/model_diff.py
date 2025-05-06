9575f7d8b2afce94122bcf6141a63bfc01f207f2
turboderp <11859846+turboderp@users.noreply.github.com>
2025-05-06 20:23:47 +0200

    model_diff.py: Add KLD and top-K test to

--- eval/model_diff.py
+++ eval/model_diff.py
@@ -2,10 +2,10 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import argparse
-from exllamav3.util.file import disk_lru_cache, disk_lru_cache_clear
+from exllamav3.util.file import disk_lru_cache
 from exllamav3.util.progress import ProgressBar
 from exllamav3.util.memory import free_mem
-from exllamav3 import Config, Model, Cache, Tokenizer
+from exllamav3 import Config, Model, Tokenizer
 from datasets import load_dataset
 import torch
 import torch.nn.functional as F
@@ -85,8 +85,105 @@ def main(args):
             md = ((sa.max().item()) / torch.linalg.norm(sb, 'fro').mean()).item()
             max_diff = max(max_diff, md)
 
+        del sa, sb
         rfn_error = rfn_error_sum / rows
         print(f" -- {module_a.key:40}   error: {rfn_error:.6f}   max_diff/norm: {max_diff:.6f}")
+
+    # Compare logits
+    topk_max = args.topk_max
+    logprob_sum = [0, 0]
+    logprob_count = [0, 0]
+    kl_div_sum_ab = 0
+    kl_div_sum_ba = 0
+    topk_hits_sum = [[0] * topk_max, [0] * topk_max]
+    topk_hits_count = [[0] * topk_max, [0] * topk_max]
+    topk_agreement_sum = [0] * topk_max
+    topk_agreement_count = [0] * topk_max
+
+    def ppl(input_ids_, logits_):
+        nonlocal logprob_sum, logprob_count
+        logprob_sum_ = 0.0
+        logprob_count_ = 0
+        chunksize = logits_.shape[1] * 10240 // logits_.shape[1]
+        b_ = 0
+        while b_ < logits_.shape[1]:
+            a_ = b_
+            b_ = min(b_ + chunksize, logits_.shape[1])
+            logits_f = logits_[a_:b_, :].float() + 1e-10
+            target_ids = input_ids_[a_ + 1:b_ + 1].to(logits_.device)
+            log_probs = F.log_softmax(logits_f, dim=-1)
+            token_log_probs = log_probs.gather(-1, target_ids.unsqueeze(-1)).squeeze(-1)
+            logprob_sum_ += token_log_probs.sum().item()
+            logprob_count_ += target_ids.numel()
+        return logprob_sum_, logprob_count_
+
+    rows = state_a.shape[0]
+    for j in range(rows):
+        x = (state_a[j], state_b[j])
+        input_ids = eval_ids[j]
+        top_indices = []
+
+        for i in [0, 1]:
+            logits = x[i][:-1, :]
+            logprob_sum__, logprob_count__ = ppl(input_ids, logits)
+            logprob_sum[i] += logprob_sum__
+            logprob_count[i] += logprob_count__
+
+            _, top_index = torch.topk(logits, topk_max, dim = -1)
+            top_index = top_index.cpu().view(-1, topk_max)
+            top_indices.append(top_index)
+            targets = input_ids[1:].view(-1, 1)
+
+            for t in range(topk_max):
+                top_slice = top_index[:, :t + 1]
+                hits = torch.eq(targets, top_slice)
+                row_hits = hits.any(dim = 1)
+                topk_hits_sum[i][t] += row_hits.sum().item()
+                topk_hits_count[i][t] += top_slice.shape[0]
+
+        for t in range(topk_max):
+            top_slice_a = top_indices[0][:, :t + 1]
+            top_slice_b = top_indices[1][:, :t + 1]
+            hits = torch.eq(top_slice_a, top_slice_b)
+            row_hits = hits.all(dim = 1)
+            topk_agreement_sum[t] += row_hits.sum().item()
+            topk_agreement_count[t] += top_slice_a.shape[0]
+
+        epsilon = 1e-10
+        probs_a = torch.softmax(x[0].float(), dim = -1)
+        probs_b = torch.softmax(x[1].float(), dim = -1)
+        kl_div = F.kl_div(torch.log(probs_a + epsilon), probs_b, reduction = 'none')
+        kl_div_sum_ab += kl_div.sum(dim = -1).mean().item()
+        kl_div = F.kl_div(torch.log(probs_b + epsilon), probs_a, reduction = 'none')
+        kl_div_sum_ba += kl_div.sum(dim = -1).mean().item()
+
+    perplexity = [math.exp(-logprob_sum[i] / logprob_count[i]) for i in (0, 1)]
+    kl_div_ab = kl_div_sum_ab / rows
+    kl_div_ba = kl_div_sum_ba / rows
+
+    # Perplexity for each model
+    print(f" -- A perplexity: {perplexity[0]:11.8f}")
+    print(f" -- B perplexity: {perplexity[1]:11.8f}")
+
+    # Probability of the test label being in the top K tokens, for each model
+    print(f" -- A label in top-K:")
+    for t in range(topk_max):
+        a_acc_ = topk_hits_sum[0][t] / topk_hits_count[0][t]
+        print(f"      K = {t+1}: {a_acc_:6.4f}")
+    print(f" -- B label in top-K:")
+    for t in range(topk_max):
+        a_acc_ = topk_hits_sum[1][t] / topk_hits_count[1][t]
+        print(f"      K = {t+1}: {a_acc_:6.4f}")
+
+    # Probability of exact top-K token match between models
+    print(f" -- Top-K agreement, A vs B:")
+    for t in range(topk_max):
+        topk_agree_ = topk_agreement_sum[t] / topk_agreement_count[t]
+        print(f"      K = {t+1}: {topk_agree_:6.4f}")
+
+    # KLD, either way around
+    print(f" -- KL divergence (A, B): {kl_div_ab:11.8f}")
+    print(f" -- KL divergence (B, A): {kl_div_ba:11.8f}")
 
 
 if __name__ == "__main__":
@@ -95,5 +192,7 @@ if __name__ == "__main__":
     parser.add_argument("-mb", "--model_b", type = str, help = "Model B", required = True)
     parser.add_argument("-r", "--rows", type = int, help = "Number of rows", default = 100)
     parser.add_argument("-kb", "--keep_b", type = int, help = "Maintain B state for number of modules", default = 0)
+    parser.add_argument("-tkm", "--topk_max", type = int, default = 5, help = "Max top-K interval to test")
+
     _args = parser.parse_args()
     main(_args)
