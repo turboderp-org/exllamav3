544ba423bf9c58c940dca6cb36a1aa93f6cda41a
turboderp <11859846+turboderp@users.noreply.github.com>
2025-07-15 21:15:47 +0200

    Add Exaone4 architecture

--- exllamav3/models/architectures.py
+++ exllamav3/models/architectures.py
@@ -4,6 +4,7 @@ from .decilm import DeciLMModel
 from .dots1 import Dots1Model
 from .ernie4_5 import Ernie4_5Model
 from .ernie4_5_moe import Ernie4_5MoEModel
+from .exaone4 import Exaone4Model
 from .gemma2 import Gemma2Model
 from .gemma3 import Gemma3Model, Gemma3TextModel
 from .glm4 import Glm4Model
@@ -30,6 +31,7 @@ ARCHITECTURES = {
         Dots1Model,
         Ernie4_5Model,
         Ernie4_5MoEModel,
+        Exaone4Model,
         Gemma2Model,
         Gemma3Model,
         Gemma3TextModel,
