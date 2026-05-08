37bdd97de45f4fbe871ae60ae9b65322917d7f31
Alex Hunt <alex.s.hunt7@gmail.com>
2026-05-08 21:47:11 +0100

    fix race in cancellation

--- exllamav3/generator/async_generator.py
+++ exllamav3/generator/async_generator.py
@@ -57,7 +57,8 @@ class AsyncGenerator:
             pass
 
     async def cancel(self, job: AsyncJob):
-        assert job.job in self.jobs
+        if job.job not in self.jobs:
+            return
         self.generator.cancel(job.job)
         del self.jobs[job.job]
 
@@ -90,5 +91,5 @@ class AsyncJob:
                 break
 
     async def cancel(self):
-        await self.generator.cancel(self)
         self.cancelled = True
+        await self.generator.cancel(self)
